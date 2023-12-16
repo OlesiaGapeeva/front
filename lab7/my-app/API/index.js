50739ff6b179c2f1f745ab25f8771a5e2057c5e8
OlesiaGapeeva <92272887+OlesiaGapeeva@users.noreply.github.com>
2023-12-16 23:36:26 +0300

    Delete lab7/my-app directory

--- lab7/my-app/API/index.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({ baseURL: 'http://192.168.124.132:8000/' });
--- lab7/my-app/store/index.js
+++
@@ -1,4 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import datareducer from './deviceSlice';
-
-export const store = configureStore({ reducer: { vacancy: datareducer } });
